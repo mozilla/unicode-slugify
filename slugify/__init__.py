305047447bc7acc42beb284e112e1e4239ff7366
Emin Bugra <eminbugrasaral@me.com>
2015-01-10 19:08:51 -0800

    Add unicode_pair parameter.

--- slugify/__init__.py
+++ slugify/__init__.py
@@ -26,7 +26,7 @@ def smart_text(s, encoding='utf-8', errors='strict'):
 SLUG_OK = '-_~'
 
 
-def slugify(s, ok=SLUG_OK, lower=True, spaces=False):
+def slugify(s, ok=SLUG_OK, lower=True, spaces=False, unicode_pairs=None):
     # L and N signify letter/number.
     # http://www.unicode.org/reports/tr44/tr44-4.html#GC_Values_Table
     rv = []
@@ -39,4 +39,9 @@ def slugify(s, ok=SLUG_OK, lower=True, spaces=False):
     new = ''.join(rv).strip()
     if not spaces:
         new = re.sub('[-\s]+', '-', new)
+
+    if unicode_pairs and isinstance(unicode_pairs, dict):
+        for char in unicode_pairs:
+            new = new.replace(char, unicode_pairs[char])
+
     return new.lower() if lower else new
