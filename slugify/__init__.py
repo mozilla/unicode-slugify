eeb291d21ec2244351a10ec88a822e12b411916b
Emin Bugra <eminbugrasaral@me.com>
2015-01-12 12:40:46 -0800

    Add docstring.

--- slugify/__init__.py
+++ slugify/__init__.py
@@ -94,14 +94,30 @@ def smart_text(s, encoding='utf-8', errors='strict'):
         s = six.text_type(s)
     return s
 
-
-# Extra characters outside of alphanumerics that we'll allow.
 SLUG_OK = '-_~'
 
 
 def slugify(s, ok=SLUG_OK, lower=True, spaces=False, replace_latin=False):
-    # L and N signify letter/number.
-    # http://www.unicode.org/reports/tr44/tr44-4.html#GC_Values_Table
+    """
+    Creates a unicode slug for given string with several options.
+
+    L and N signify letter/number.
+    http://www.unicode.org/reports/tr44/tr44-4.html#GC_Values_Table
+
+    :param s: Your unicode string.
+    :param ok: Extra characters outside of alphanumerics to be allowed.
+    :param lower: Lower the output string.
+    :param spaces: True allows spaces, False replaces a space with a dash (-)
+    :param replace_latin: True to replace common unicode latin letters with their similar ascii representations.
+    :type s: String
+    :type ok: String
+    :type lower: Bool
+    :type spaces: Bool
+    :type replace_latin: Bool
+    :return: Slugified unicode string
+
+    """
+
     rv = []
     for c in unicodedata.normalize('NFKC', smart_text(s)):
         cat = unicodedata.category(c)[0]
