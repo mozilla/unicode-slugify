d61cdecdf061a0e3583d86c8c7b9c5b520e87a9d
nitely <ecastroborsani@gmail.com>
2017-11-02 21:37:56 -0300

    bump minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='unicode-slugify',
-    version='0.1.3',
+    version='0.1.4',
     description='A slug generator that turns strings into unicode slugs.',
     long_description=open('README.md').read(),
     author='Jeff Balogh, Dave Dash',
